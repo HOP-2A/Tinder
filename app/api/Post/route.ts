ac16ed4acc8943531f40aed1699bd7d395d2302c
Enkhsuvd Dorjsuren <enkhsuvddorjsuren@Enkhsuvds-MacBook-Pro.local>
2025-12-02 16:44:30 +0800

    nile yum hise

--- app/api/Post/route.ts
+++ app/api/Post/route.ts
@@ -1,21 +1,24 @@
-import { prisma } from "@/lib/db"
-import { NextResponse } from "next/server"
-export async function POST(req:Request) {
-    const body = await req.json()
-    const {images, caption,userId} = body
-    console.log(body);
-    console.log(userId);
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { images, caption, userId } = body;
+  console.log(body);
+  console.log(userId);
 
-
-    if(!images || !caption || !userId){
-        return NextResponse.json({message:"duttsaaa"},{status:400})
-    }
-await prisma.post.create({
-        data:{
-            images:Array.isArray(images) ? images : [images],
-            caption,
-            userId,
-        }
-    })
-    return NextResponse.json({message:"post created"},{status:200})
-}   
+  if (!images || !caption || !userId) {
+    return NextResponse.json({ message: "duttsaaa" }, { status: 400 });
+  }
+  await prisma.post.create({
+    data: {
+      images: Array.isArray(images) ? images : [images],
+      caption,
+      userId,
+    },
+  });
+  return NextResponse.json({ message: "post created" }, { status: 200 });
+}
+export async function GET() {
+  const allPost = await prisma.post.findMany();
+  return NextResponse.json({ message: allPost }, { status: 200 });
+}
