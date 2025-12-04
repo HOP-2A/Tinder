284730f76802701bc92458f6a5a2504e6e4ae0d4
Lkhagvadorj-3 <amarkhuulkhagvadorj00@gmail.com>
2025-12-04 15:11:57 +0800

    gg

--- app/api/User/signup/route.ts
+++ app/api/User/signup/route.ts
@@ -1,39 +1,44 @@
-import { prisma } from "@/lib/db"
-import bcrypt from "bcrypt"
-import { NextResponse } from "next/server"
+// import { prisma } from "@/lib/db"
+// import bcrypt from "bcrypt"
+// import { NextResponse } from "next/server"
 
-export async function POST(req:Request) {
-    const {username,firstName, lastName, email,password}= await req.json()
-    if(!username || !email || !password){
-        return NextResponse.json("all fields should be filled")
-    }
+import { NextResponse } from "next/server";
 
-    const existingEmail =await prisma.user.findUnique({
-        where:{email}
-    })
+// export async function POST(req:Request) {
+//     const {username,firstName, lastName, email,password}= await req.json()
+//     if(!username || !email || !password){
+//         return NextResponse.json("all fields should be filled")
+//     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+//     const existingEmail =await prisma.user.findUnique({
+//         where:{email}
+//     })
 
-        if (existingEmail) {
-  return NextResponse.json(
-    { message: "email existing" },
-    { status: 400 }
-  );
-}
+//     const hashedPassword = await bcrypt.hash(password,10);
 
+//         if (existingEmail) {
+//   return NextResponse.json(
+//     { message: "email existing" },
+//     { status: 400 }
+//   );
+// }
 
-    const NewUser = await  prisma.user.create({
-        data:{
-            username,   
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword
-        }
-    })
-    console.log(NewUser);
-    
-    return NextResponse.json({message:"amjilttai"},{status:200})
+//     const NewUser = await  prisma.user.create({
+//         data:{
+//             username,
+//             firstName,
+//             lastName,
+//             email,
+//             password: hashedPassword
+//         }
+//     })
+//     console.log(NewUser);
 
-    // return NextResponse.json({message:NewUser},{status:200})   
-}
+//     return NextResponse.json({message:"amjilttai"},{status:200})
+
+//     // return NextResponse.json({message:NewUser},{status:200})
+// }
+
+export const GET = () => {
+  return NextResponse.json("gg");
+};
